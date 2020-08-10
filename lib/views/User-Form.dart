f3427b307011ecbcbc22c9f02a5eddd283576316
Paulo Henrique <paulohenrique@MacBook-Air-de-Paulo.local>
2020-08-09 22:35:36 -0300

    Implemented edit user data form

--- lib/views/User-Form.dart
+++ lib/views/User-Form.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:user_list/models/User.dart';
+import 'package:user_list/provider/Users.dart';
 
 class UserForm extends StatelessWidget {
 
   final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
+  void _loadFormData(User user){
+    _formData['id'] = user.id;
+    _formData['name'] = user.name;
+    _formData['email'] = user.email;
+    _formData['avatarUrl'] = user.avatarUrl;
+
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    final User user = ModalRoute.of(context).settings.arguments;
+
+    _loadFormData(user);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Form user"),
@@ -17,6 +34,12 @@ class UserForm extends StatelessWidget {
               
               if(isValid){
                 _form.currentState.save();
+                Provider.of<Users>(context, listen: false).put(User(
+                  id: _formData['id'],
+                  name: _formData['name'],
+                  email: _formData['email'],
+                  avatarUrl: _formData['avatarUrl'],
+                ));
                 Navigator.of(context).pop();
               }
 
@@ -30,6 +53,7 @@ class UserForm extends StatelessWidget {
           child: Column(
             children: <Widget>[
               TextFormField(
+                initialValue: _formData['name'],
                 validator: (value){
                   if(value == null || value.isEmpty){
                     return 'Include your name';
@@ -39,15 +63,17 @@ class UserForm extends StatelessWidget {
                     return 'Name too small';
                   }
                 },
-                onSaved: (value){
-                  print(value);
-                },
+                onSaved: (value) => _formData['name'] = value,
                 decoration: InputDecoration(labelText: 'Name'),
               ),
               TextFormField(
+                initialValue: _formData['email'],
+                onSaved: (value) => _formData['email'] = value,                
                 decoration: InputDecoration(labelText: 'e-mail'),
               ),
               TextFormField(
+                initialValue: _formData['avatarUrl'],
+                onSaved: (value) => _formData['avatarUrl'] = value,
                 decoration: InputDecoration(labelText: 'Avatar URL'),
               ),
             ],
